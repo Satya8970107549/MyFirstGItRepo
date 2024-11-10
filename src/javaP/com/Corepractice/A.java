3728054dd22e7c57cc125290953f45e009feb449
Satya Yadav <satyad97@in.ibm.com>
2024-11-10 13:17:16 +0530

    new project

---
+++ src/javaP/com/Corepractice/A.java
@@ -0,0 +1,47 @@
+package javaP.com.Corepractice;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class A {
+	
+	public static void checkUniqueString(String s) {
+		
+		// 1st way
+		
+		char[] chars = s.toCharArray();
+//		
+//		HashSet hs =new HashSet();
+//		for(char i : chars) {
+//			hs.add(i);
+//			
+//		}
+//		if(hs.size() == chars.length) {
+//			System.out.println("Input String "+s +" is unique");
+//		}
+//		else {
+//			System.out.println("not unique");
+//		}
+		
+		// 2nd way 
+		
+		syso
+		for(int i =0 ; i<= chars.length ; i++) {
+			
+			if(chars[i] != chars[i+1]) {
+				
+				
+			}
+			
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//checkUniqueString("saty");
+
+	}
+
+}
